ca0e4500e7090e17ec4edcc0e242a0afef69041e
StefanvanTilburg <TillyAliasStefan@gmail.com>
2020-11-18 12:14:59 +0100

    Added competitionForm, Changed CompetitionController, now able to add competitions

--- src/main/java/nl/competitie/badminton/BadmintonCompetitie/controller/CompetitionController.java
+++ src/main/java/nl/competitie/badminton/BadmintonCompetitie/controller/CompetitionController.java
@@ -1,10 +1,14 @@
 package nl.competitie.badminton.BadmintonCompetitie.controller;
 
+import nl.competitie.badminton.BadmintonCompetitie.model.Competition;
 import nl.competitie.badminton.BadmintonCompetitie.repository.CompetitionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * @author Stefan van Tilburg <s.g.van.tilburg@st.hanze.nl>
@@ -21,5 +25,22 @@ public class CompetitionController {
     protected String showCompetition(Model model) {
         model.addAttribute("allCompetitions", competitionRepository.findAll());
         return "competitionOverview";
+    }
+
+    @GetMapping("/competition/add")
+    protected String showCompetitionForm(Model model) {
+        model.addAttribute("competition", new Competition());
+        return "competitionForm";
+    }
+
+    @PostMapping("/competition/add")
+    protected String saveOrUpdateCompetition(@ModelAttribute("competition") Competition competition,
+                                             BindingResult result) {
+        if (result.hasErrors()) {
+            return "competitionForm";
+        } else {
+            competitionRepository.save(competition);
+            return "redirect:/Competition";
+        }
     }
 }
